d1b2c942b309bd37b3f4c8ccbf19175cd57f2ad2
Alhaitham Al Jabri <jabri.dev@gmail.com>
2022-03-15 20:58:36 +0400

    Update dot_doom.d/init.el

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -1,8 +1,5 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-;; This file controls what Doom modules are enabled and what order they load
-;; in. Remember to run 'doom sync' after modifying it!
-
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
 ;;      documentation. There you'll find a "Module Index" link where you'll find
 ;;      a comprehensive list of Doom's modules and what flags they support.
