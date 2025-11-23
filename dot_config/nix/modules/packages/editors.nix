459abb74c864427f0289eb5b16c41d8a272cdef3
Alhaitham Al Jabri <jabri.dev@gmail.com>
2025-11-23 13:27:54 +0400

    Update .config/nix/modules/packages/editors.nix

--- dot_config/nix/modules/packages/editors.nix
+++ dot_config/nix/modules/packages/editors.nix
@@ -6,4 +6,7 @@ with pkgs;
   emacs
   libreoffice-qt6-still
   font-awesome
+  tree-sitter-grammars.tree-sitter-typescript
+  tree-sitter-grammars.tree-sitter-tsx
+  tree-sitter-grammars.tree-sitter-javascript
 ]
